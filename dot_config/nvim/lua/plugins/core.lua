3d68144067841b13797a8e9644198f2b7e034e5b
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2025-11-18 08:57:35 +0800

    Added starter nvim config
    A

---
+++ dot_config/nvim/lua/plugins/core.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "snacks.nvim",
+    opts = {
+      bigfile = { enabled = true },
+      quickfile = { enabled = true },
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = true,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        middle_mouse_command = function(n)
+          Snacks.bufdelete(n)
+        end,
+        always_show_bufferline = true,
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        view = "cmdline",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "rust", "ron", "zig", "go" } },
+  },
+}
