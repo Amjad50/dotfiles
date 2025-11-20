1f90ac09fe77036639ad6514172792c10a43a16c
Amjad Alsharafi <26300843+Amjad50@users.noreply.github.com>
2025-11-20 15:01:59 +0800

    nvim: Use `<CR>` to select completion as well

--- dot_config/nvim/lua/plugins/core.lua
+++ dot_config/nvim/lua/plugins/core.lua
@@ -17,6 +17,17 @@ return {
     opts = {
       keymap = {
         preset = "super-tab",
+        ["<CR>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          "snippet_forward",
+          "fallback",
+        },
       },
       completion = {
         trigger = {
